81e27f645c0a983686b44e6f0f33a09caaf22252
RadikFatkhriev <xfess@protonmail.ch>
2022-04-26 09:44:00 -0400

    remove node

--- vars/myfuncs.groovy
+++ vars/myfuncs.groovy
@@ -1,7 +1,5 @@
 package vars
 
 def setCustomBuildId(
-    node {
-        buildName "#${env.BUILD_NUMBER}-${currentBuild.getBuildCauses()[0].userId}"
-    }
+    buildName "#${env.BUILD_NUMBER}-${currentBuild.getBuildCauses()[0].userId}"
 )
