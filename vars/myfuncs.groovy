696d76f7de48fd4ec783780b714be5fbacd47646
RadikFatkhriev <xfess@protonmail.ch>
2022-04-26 10:05:51 -0400

    up

--- vars/myfuncs.groovy
+++ vars/myfuncs.groovy
@@ -7,3 +7,9 @@ def simpleHello() {
 def setCustomBuildId() {
     buildName "#${env.BUILD_NUMBER}-${currentBuild.getBuildCauses()[0].userId}"
 }
+
+def setCustomBuildIdInsideNode() {
+    node {
+        buildName "#${env.BUILD_NUMBER}-${currentBuild.getBuildCauses()[0].userId}"
+    }   
+}
