fb53fd6d24d1061531529660adf453e63591ae97
RadikFatkhriev <xfess@protonmail.ch>
2022-04-27 10:22:55 -0400

    up

--- vars/myfuncs.groovy
+++ vars/myfuncs.groovy
@@ -4,10 +4,6 @@ def simpleHello() {
     println 'It is msg from myfuncs'
 }
 
-def setCustomBuildId0() {
-    buildName "#${env.BUILD_NUMBER}_${currentBuild.getBuildCauses()[0].userId}"
-}
-
 def setCustomBuildId() {
     if ("${currentBuild.getBuildCauses()[0].userId}" != 'null') {
         buildName "#${env.BUILD_NUMBER}_${currentBuild.getBuildCauses()[0].userId}"
